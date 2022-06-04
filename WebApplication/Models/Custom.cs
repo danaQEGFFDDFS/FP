e561071dfbc8a7195421721c3b9392b4c0111b00
danaQEGFFDDFS <99499448+danaQEGFFDDFS@users.noreply.github.com>
2022-06-04 22:55:31 +0300

    Add files via upload

---
+++ WebApplication/Models/Custom.cs
@@ -0,0 +1,24 @@
+﻿using DataLayer;
+using DataLayer.Entityes;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.Logging;
+using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations.Schema;
+using System.Diagnostics;
+using System.Linq;
+using System.Threading.Tasks;
+using WebApplication.Models;
+
+namespace WebApplication.Models
+{
+    public class Custom
+    {
+        public int Id { get; set; }
+        public string FullName { get; set; }
+        public long Phone { get; set; }
+        public string Address { get; set; }
+       
+    }
+}
