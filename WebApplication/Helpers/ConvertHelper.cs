36b2a40f31b57bd791dad734ef12c483166c6054
danaQEGFFDDFS <99499448+danaQEGFFDDFS@users.noreply.github.com>
2022-06-05 12:24:05 +0300

    Add files via upload

---
+++ WebApplication/Helpers/ConvertHelper.cs
@@ -0,0 +1,35 @@
+﻿using DataLayer;
+using Microsoft.AspNetCore;
+using Microsoft.AspNetCore.Hosting;
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.DependencyInjection;
+using Microsoft.Extensions.Hosting;
+using Microsoft.Extensions.Logging;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using WebApplication.Models;
+using DataLayer.Entityes;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+
+
+namespace WebApplication.Helpers
+{
+    public static class ConvertHelper
+    {
+        public static Custom ToCustom(this Customers custom)
+        {
+            return new Custom()
+            {
+               FullName = custom.FullName,
+               Address = custom.Address,
+               Phone = custom.Phone
+            };
+
+
+        }
+
+    }
+}
